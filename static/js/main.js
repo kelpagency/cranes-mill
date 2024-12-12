684b5fed70f10892f313c5d9c785a7e26525c46a
judahholland <judahrobert123@gmail.com>
2024-12-12 18:58:35 -0500

    Updated nav bar

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
   // *********************
   // Hamburger menu
   // *********************
+
   const sidebar = document.querySelector(".sidebar")
 
   const closeSidebar = document.querySelector(".close-btn")
@@ -18,10 +19,42 @@ document.addEventListener('DOMContentLoaded', function () {
   const linkChev = document.querySelectorAll(".link-chev");
 
   linkChev.forEach(function (item) {
-    item.addEventListener("click", (e) => {
+    if (window.innerWidth < 900) {
+      item.addEventListener("click", (e) => {
       item.parentElement.classList.toggle("active");
-    })
+      })
+    }
   })
+
+  const menuItem = document.querySelectorAll(".menu-item")
+
+  menuItem.forEach(function (item) {
+    if (window.innerWidth > 900) {
+      item.addEventListener("mouseenter", () => {
+        item.classList.add("active");
+      })
+      item.addEventListener("mouseleave", () => {
+        item.classList.remove("active");
+      })
+    }
+  })
+
+  function handleSidebar() {
+    const hamburgerMenu = document.querySelector(".hamburger-menu")
+    const desktopMenu = document.querySelector(".header-navigation")
+
+    if (window.innerWidth > 900) {
+      hamburgerMenu.classList.add("hide");
+      desktopMenu.classList.remove("hide");
+    }
+    if (window.innerWidth < 900) {
+      hamburgerMenu.classList.remove("hide");
+      desktopMenu.classList.add("hide");
+    }
+  }
+
+  handleSidebar();
+  window.addEventListener('resize', handleSidebar);
   // *********************
   // Blog comments
   // reply to comment button
