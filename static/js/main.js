65059a0bbd0ae2893e7f575de11d70f10c1291ba
Stefen Phelps <stefen@kelp.agency>
2025-01-07 15:52:18 -0500

    add publishing info to nova settings

--- static/js/main.js
+++ static/js/main.js
@@ -1,48 +1,47 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   // *********************
   // Hamburger menu
   // *********************
 
+  const sidebar = document.querySelector(".sidebar");
 
-  const sidebar = document.querySelector(".sidebar")
+  const closeSidebar = document.querySelector(".close-btn");
 
-  const closeSidebar = document.querySelector(".close-btn")
-
-  const showSidebar = document.querySelector(".menu-button")
+  const showSidebar = document.querySelector(".menu-button");
 
   showSidebar.addEventListener("click", () => {
     sidebar.classList.add("show");
-  })
+  });
   closeSidebar.addEventListener("click", () => {
     sidebar.classList.remove("show");
-  })
+  });
 
   const linkChev = document.querySelectorAll(".link-chev");
 
   linkChev.forEach(function (item) {
     if (window.innerWidth < 900) {
       item.addEventListener("click", (e) => {
-      item.parentElement.classList.toggle("active");
-      })
+        item.parentElement.classList.toggle("active");
+      });
     }
-  })
+  });
 
-  const menuItem = document.querySelectorAll(".menu-item")
+  const menuItem = document.querySelectorAll(".menu-item");
 
   menuItem.forEach(function (item) {
     if (window.innerWidth > 900) {
       item.addEventListener("mouseenter", () => {
         item.classList.add("active");
-      })
+      });
       item.addEventListener("mouseleave", () => {
         item.classList.remove("active");
-      })
+      });
     }
-  })
+  });
 
   function handleSidebar() {
-    const hamburgerMenu = document.querySelector(".hamburger-menu")
-    const desktopMenu = document.querySelector(".header-navigation")
+    const hamburgerMenu = document.querySelector(".hamburger-menu");
+    const desktopMenu = document.querySelector(".header-navigation");
 
     if (window.innerWidth > 900) {
       hamburgerMenu.classList.add("hide");
@@ -55,16 +54,16 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   handleSidebar();
-  window.addEventListener('resize', handleSidebar);
+  window.addEventListener("resize", handleSidebar);
   // *********************
   // Blog comments
   // reply to comment button
-  var commentReplyButtons = document.querySelectorAll('.comment-form-button');
+  var commentReplyButtons = document.querySelectorAll(".comment-form-button");
   if (commentReplyButtons) {
     commentReplyButtons.forEach(function (item) {
-      item.addEventListener('click', function () {
-        item.nextElementSibling.classList.remove('hidden');
-        item.nextElementSibling.querySelector('textarea').focus();
+      item.addEventListener("click", function () {
+        item.nextElementSibling.classList.remove("hidden");
+        item.nextElementSibling.querySelector("textarea").focus();
         item.remove();
       });
     });
